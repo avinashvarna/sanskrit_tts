0946a68a2a66edc915b75de21cfb3acf34c60c42
Avinash Varna <avinashvarna@users.noreply.github.com>
2023-01-03 05:37:20 +0000

    Added app engine files and some minor updates

---
+++ app_engine/api_v1.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# import logging
+import os
+
+from flask import Blueprint, make_response
+from flask_restx import Resource, Api, reqparse
+
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript.schemes import VisargaApproximation
+
+import sanskrit_tts
+from sanskrit_tts.bhashini_tts import BhashiniTTS, BhashiniVoice
+
+
+URL_PREFIX = "/v1"
+api_blueprint = Blueprint("sanskrit_tts", __name__)
+
+api = Api(
+    app=api_blueprint,
+    version="1.0",
+    title="sanskrit_tts API",
+    description='For detailed intro and to report issues: see <a href="https://github.com/avinashvarna/sanskrit_tts">here</a>. '
+    'A list of REST and non-REST API routes avalilable on this server: <a href="../sitemap">sitemap</a>.',
+    default_label=api_blueprint.name,
+    prefix=URL_PREFIX,
+    doc="/docs",
+)
+
+
+api_key = os.environ.get("BHASHINI_API_KEY", None)
+if api_key is None:
+    # TODO
+    raise Exception("Bhashini API key not found")
+
+
+@api.route("/version/")
+class Version(Resource):
+    """sanskrit_tts Library Version"""
+    def get(self):
+        """sanskrit_tts Library Version"""
+        response = {"version": str(sanskrit_tts.__version__)}
+        return response
+
+
+synthesize_parser = reqparse.RequestParser()
+synthesize_parser.add_argument(
+    "text", type=str, required=True, help="The text to be synthesized"
+)
+synthesize_parser.add_argument(
+    "input_encoding",
+    type=str,
+    default=None,
+    help="Encoding of the text",
+    choices=list(sanscript.SCHEMES.keys()),
+)
+synthesize_parser.add_argument(
+    "voice",
+    type=int,
+    default=2,
+    help="The voice to use",
+    choices=[x.value for x in BhashiniVoice.__members__.values()],
+)
+synthesize_parser.add_argument(
+    "visarga_approximation",
+    type=int,
+    default=0,
+    help="Visarga approximation to use",
+    choices=[x.value for x in VisargaApproximation.__members__.values()],
+)
+
+
+@api.route("/synthesize/")
+# @api.response(200, ['audio/mpeg'])
+@api.produces("audio/mpeg")
+@api.expect(synthesize_parser, validate=True)
+class Synthesize(Resource):
+    """Synthesis handler"""
+
+    def post(self):
+        """Handle POST"""
+        args = synthesize_parser.parse_args()
+        tts = BhashiniTTS(
+            voice=BhashiniVoice(args.voice),
+            api_key=api_key,
+        )
+        tts_response = tts._synthesis_response(
+            args.text, args.input_encoding, args.visarga_approximation
+        )
+        response = make_response(tts_response.content)
+        response.headers.set("Content-Type", "audio/mpeg")
+        return response
---
+++ app_engine/test_bhashini_proxy_api.py
@@ -0,0 +1,28 @@
+""" Test Bhashini Proxy API"""
+from io import BytesIO
+
+from indic_transliteration.sanscript import DEVANAGARI
+from indic_transliteration.sanscript.schemes import VisargaApproximation
+from pydub import AudioSegment
+
+from sanskrit_tts.bhashini_tts import BhashiniVoice
+from main import app
+
+
+def test_bhashini_proxy_api():
+    """ Test Bhashini Proxy API"""
+    text = "कालिदासस्य जीवनवृत्तिविषये अनेकाः लोकविश्रुतयः अनेके वादाः च सन्ति ।"
+    client = app.test_client()
+    url = "/v1/synthesize/"
+    data = {
+        "text": text,
+        "input_encoding": DEVANAGARI,
+        "voice": int(BhashiniVoice.FEMALE2),
+        "visarga_approximation": int(VisargaApproximation.AHA),
+    }
+    test_response = client.post(url, json=data)
+    assert "Content-Type" in test_response.headers
+    assert test_response.headers["Content-Type"] == "audio/mpeg"
+    response = test_response.data
+    audio = AudioSegment.from_file(BytesIO(response))
+    audio.export("proxy.mp3")
